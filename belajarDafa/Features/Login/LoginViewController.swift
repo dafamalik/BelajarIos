f9782daf28bbb7c24adff9125cb853fb9c4746ab
dafamalik <dafamalik08@gmail.com>
2022-05-24 14:27:30 +0700

    belajar ui image, button, text, textfield

---
+++ belajarDafa/Features/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  belajarDafa
+//
+//  Created by dafamalik on 24/05/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btnLogin: UIButton!
+    
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+    }
+    
+    func setupViews() {
+        
+        lblTitle.textColor = .black
+        btnLogin.layer.cornerRadius = 8
+        tfEmail.layer.cornerRadius = 8
+        tfPassword.layer.cornerRadius = 8
+    }
+
+    @IBAction func login(_ sender: UIButton) {
+        print("email yang diinput adalah",tfEmail.text ?? "")
+        print("password yang diinput adalah",tfPassword.text ?? "")
+    }
+}
